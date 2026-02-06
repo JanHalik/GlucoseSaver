092c957e09f4eb6bf1a31a3e227a2ca9952d350f
Jan Halik <halik.jan@gmail.com>
2026-02-06 13:13:22 +0100

    Write into CSV file

--- PollingGlukose.py
+++ PollingGlukose.py
@@ -1,3 +1,4 @@
+import os
 import time
 from datetime import datetime
 from pylibrelinkup import PyLibreLinkUp
@@ -9,7 +10,7 @@ PASSWORD = "MHDiaObvodova1"
 
 # Interval v sekundách (10 minut)
 INTERVAL = 600
-
+CSV_FILE = "glucose.csv"
 def main():
     client = PyLibreLinkUp(email=EMAIL, password=PASSWORD)
     client.authenticate()
@@ -21,22 +22,41 @@ def main():
     patient_id = patients[0].patient_id 
 
     print(f"Sleduji pacienta: {patients[0].first_name} {patients[0].last_name}")
+    csv=[]
+    # 1️⃣ Načtení existujícího CSV při startu
+    if not os.path.exists(CSV_FILE):
+        # with open(CSV_FILE, "r", encoding="utf-8") as f:
+        #     csv = [line.strip() for line in f if line.strip()]
+    #else:
+        # hlavička – zapíše se jen při prvním spuštění
+        csv.append("timestamp,glucose,unit")
+    try:
+        while True:
+            try:
+                data = client.latest(patient_identifier=patient_id)
+                print(data)
 
-    while True:
-        try:
-            data = client.latest(patient_identifier=patient_id)
-            print(data)
+                now = datetime.now().isoformat(timespec="seconds")
 
-            now = datetime.now().isoformat(timespec="seconds")
+                print(f"{data},mol/l")
+                timestamp=data.timestamp
+                value=data.value
+                csv.append(f"{timestamp},{value},mol/l")
 
-            print(
-                f"[{now}] Glukóza: {data} mmol/l"
-            )
+            except Exception as e:
+                print(f"[{datetime.now().isoformat(timespec='seconds')}] Chyba: {e}")
 
-        except Exception as e:
-            print(f"[{datetime.now().isoformat(timespec='seconds')}] Chyba: {e}")
+            time.sleep(INTERVAL)
+    except KeyboardInterrupt:
+        print("Ukončení pomocí Ctrl+C")
 
-        time.sleep(INTERVAL)
+    finally:
+        with open(CSV_FILE, "a", encoding="utf-8") as f:
+            for line in csv:
+                f.write(line + "\n")
+
+        print(f"CSV uloženo do souboru: {CSV_FILE}")
+
 
 
 if __name__ == "__main__":
