024a1b09117ea264a3b0aa2b2137ef85bfccc6f6
Jan Halik <halik.jan@gmail.com>
2026-04-15 10:31:16 +0200

    Prepare Dockerfiles

---
+++ DockerImages/Glucosestart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Spouštím aplikaci..."
+# start nginx
+nginx
+# Můžeš provést migrace nebo jiné úkoly:
+alembic upgrade head
+
+# Spuštění FastAPI (uvicorn)
+uvicorn glucose_viewer.main:app --app-dir src --log-config Logging.yaml --host 127.0.0.1 --port 8087
+echo "Uvicorn glucose-api is running on port 8087"
---
+++ DockerImages/buildGlucoseImage.sh
@@ -0,0 +1,1 @@
+docker build -f DockerImages/GlucoseDockerfile -t glucose_api .
